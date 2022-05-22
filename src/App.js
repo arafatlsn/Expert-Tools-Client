e6137968c40c95cb2b9098d916b12f9cc0388b69
Arafat37 <arafatsarkar22@gmail.com>
2022-05-22 14:25:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import HomePage from './COMPONENTS/HOME PAGE/HomePage';
+import NavbarComp from './COMPONENTS/NAVBAR/NavbarComp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='lg:w-[75%] mx-auto'>
+      <NavbarComp></NavbarComp>
+        <Routes>
+          <Route path='/' element={<HomePage></HomePage>}></Route>
+        </Routes>
     </div>
   );
 }
---
+++ src/COMPONENTS/HOME PAGE/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CarouselComp from './CarouselComp';
+
+const HomePage = () => {
+  return (
+    <div>
+      <CarouselComp></CarouselComp>
+    </div>
+  );
+};
+
+export default HomePage;
