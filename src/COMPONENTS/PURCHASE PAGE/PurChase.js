e23f67f840a73e721e51814b489ddbe56c1b21b1
Arafat37 <arafatsarkar22@gmail.com>
2022-05-24 15:24:08 +0600

    post order and update quantity

--- src/COMPONENTS/PURCHASE PAGE/PurChase.js
+++ src/COMPONENTS/PURCHASE PAGE/PurChase.js
@@ -17,17 +17,22 @@ const PurChase = () => {
   const [confirmModal, setConfirmModal] = useState(false)
   const [purchaseObj, setPurchaseObj] = useState({});
 
-  const pressOrder = async() => {
-
-    const { data } = await axios.post('http://localhost:5000/order', purchaseObj)
-
-  }
-
-  const { data: tool, isLoading } = useQuery('tool', async() => {
+  const { data: tool, isLoading, refetch: singleTool } = useQuery('tool', async() => {
     const url = `http://localhost:5000/tool?id=${id}`
     const { data } = await axios.get(url);
     return data
   })
+
+  const pressOrder = async() => {
+
+    const id = purchaseObj.toolId;
+    console.log(purchaseObj)
+    const { data } = await axios.post(`http://localhost:5000/order?id=${id}`, purchaseObj)
+    if(data.updateTool?.modifiedCount){
+      singleTool()
+    }
+
+  }
 
   if(isLoading){
     return;
@@ -86,7 +91,6 @@ const PurChase = () => {
             
             
             }} type="number" name="floating_password" id="floating_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "
-            defaultValue={minimum_order}
               />
 
               <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">How Much You Want?</label>
@@ -104,7 +108,7 @@ const PurChase = () => {
       </div>
 
       {
-        showModal && <PurchaseModal tool={tool} getQuantity={getQuantity} setShowModal={setShowModal} setConfirmModal={setConfirmModal} setPurchaseObj={setPurchaseObj} ></PurchaseModal>
+        (showModal && !getError) && <PurchaseModal tool={tool} getQuantity={getQuantity} setShowModal={setShowModal} setConfirmModal={setConfirmModal} setPurchaseObj={setPurchaseObj} ></PurchaseModal>
       }
 
       {
